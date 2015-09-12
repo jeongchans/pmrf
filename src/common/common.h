9ad87f6c9997d16804c70801ba00ff94e084521c
Chan-Seok Jeong <csjeong@kaist.ac.kr>
2015-09-12 11:14:51 +0900

    refactor: clean common directory

--- src/common/common.h
+++ src/common/common.h
@@ -15,36 +15,4 @@ using std::map;
 using std::string;
 using std::make_pair;
 
-template <typename T1, typename T2>
-static inline const pair<T1, T2>& max_pair(const pair<T1, T2>& x, const pair<T1, T2>& y) {
-    return (x.first > y.first) ? x : y;
-}
-
-template <typename T1, typename T2>
-static inline const pair<T1, T2>& max3_pair(const pair<T1, T2>& x, const pair<T1, T2>& y, const pair<T1, T2>& z) {
-    return max_pair(max_pair(x, y), z);
-}
-
-template <typename T1, typename T2>
-static inline const pair<T1, T2>& max4_pair(const pair<T1, T2>& x, const pair<T1, T2>& y, const pair<T1, T2>& z, const pair<T1, T2>& w) {
-    return max_pair(max_pair(max_pair(x, y), z), w);
-}
-
-template <typename T1, typename T2>
-static inline const pair<T1, T2>& max5_pair(const pair<T1, T2>& x, const pair<T1, T2>& y, const pair<T1, T2>& z, const pair<T1, T2>& w, const pair<T1, T2>& v) {
-    return max_pair(max_pair(max_pair(max_pair(x, y), z), w), v);
-}
-
-template <typename T1, typename T2>
-static inline const pair<T1, T2>& max6_pair(const pair<T1, T2>& x, const pair<T1, T2>& y, const pair<T1, T2>& z, const pair<T1, T2>& w, const pair<T1, T2>& v, const pair<T1, T2>& u) {
-    return max_pair(max_pair(max_pair(max_pair(max_pair(x, y), z), w), v), u);
-}
-
-class ConvergeException : public std::exception {
-  public:
-    virtual const char* what() const throw() {
-        return "Failed to converge";
-    }
-};
-
 #endif
