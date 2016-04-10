e5cd5ab3312482c72dff29a1a42d9f599b9d56c2
Chan-Seok Jeong <csjeong@kaist.ac.kr>
2016-04-10 18:31:17 +0900

    feat: write/read MRF model from a binary file
    Instead of an ascii file, we use a binary file for efficient write/read functionality.
    Pros: reduced file I/O time and file size
    Cons: not readable model file

--- src/mrfio.cpp
+++ src/mrfio.cpp
@@ -21,251 +21,64 @@ EdgeIndexVector EdgeIndexImporter::import(std::istream& is) {
 }
 
 void MRFExporter::export_model(const MRF& model, ostream& os) {
-    export_header(model, os);
-    export_body(model, os);
-}
-
-void MRFExporter::export_header(const MRF& model, ostream& os) {
-    os << "VER\t" << VERSION << endl;
-    os << "LENG\t" << model.get_length() << endl;
-    os << "SEQ" << endl;
-    export_seq(model.get_seq(), 100, os);
-}
-
-void MRFExporter::export_body(const MRF& model, ostream& os) {
-    string symbols = model.get_var_symbol();
-    string seq = model.get_seq();
+    const string sep = "\t";
+    Eigen::IOFormat fmt(4, Eigen::DontAlignCols, sep, sep);
     size_t n = model.get_length();
+    string seq = model.get_seq();
+    size_t k = model.get_num_var();
     EdgeIndexVector edge_idxs = model.get_edge_idxs();
-    os << "# PROFILE" << endl;
-    os << "RES\t";
-    export_node_symbol(symbols, os);
-    os << endl;
-    for (size_t i = 0; i < n; ++i) {
-        os << seq[i] << " " << i + 1 << "\t";
-        export_psfm(model.get_psfm(i), os);
-        os << endl;
-    }
-    os << "# NODE" << endl;
-    os << "RES\t";
-    export_node_symbol(symbols, os);
-    os << endl;
-    for (size_t i = 0; i < n; ++i) {
-        os << seq[i] << " " << i + 1 << "\t";
-        export_node_weight(model.get_node(i).get_weight(), os);
-        os << endl;
-    }
-    os << "# EDGE" << endl;
-    os << "RES1\tRES2\t";
-    export_edge_symbol(symbols, os);
-    os << endl;
+    size_t n2 = edge_idxs.size();
+    os.write((char*)&n, sizeof(size_t));
+    os.write(seq.c_str(), seq.size() + 1);
+    // Writing MRF architecture
+    os.write((char*)&n2, sizeof(size_t));
     for (EdgeIndexVector::const_iterator pos = edge_idxs.begin(); pos != edge_idxs.end(); ++pos) {
-        size_t i = pos->idx1;
-        size_t j = pos->idx2;
-        os << seq[i] << " " << i + 1 << "\t";
-        os << seq[j] << " " << j + 1 << "\t";
-        export_edge_weight(model.get_edge(i, j).get_weight(), os);
-        os << endl;
+        os.write((char*)&(pos->idx1), sizeof(size_t));
+        os.write((char*)&(pos->idx2), sizeof(size_t));
     }
-    os << "//" << endl;
-}
-
-void MRFExporter::export_seq(const string& seq, const size_t& width, ostream& os) {
-    size_t n = seq.size();
-    size_t i = 0;
-    do {
-        os << seq.substr(i, width) << endl;
-        i += width;
-    } while (i < n);
-}
-
-void MRFExporter::export_psfm(const VectorXf& w, ostream& os) {
-    export_elem(w(0), os);
-    for (size_t i = 1; i < w.size(); ++i) export_elem(w(i), os, true);
-}
-
-void MRFExporter::export_node_symbol(const string& sym, ostream& os) {
-    export_elem(sym[0], os);
-    for (size_t i = 1; i < sym.size(); ++i) export_elem(sym[i], os, true);
-}
-
-void MRFExporter::export_node_weight(const VectorXf& w, ostream& os) {
-    export_elem(w(0), os);
-    for (size_t i = 1; i < w.size(); ++i) export_elem(w(i), os, true);
-}
-
-void MRFExporter::export_edge_symbol(const string& sym, ostream& os) {
-    string s = "";
-    s = sym[0];
-    s += sym[0];
-    export_elem(s, os);
-    for (size_t i = 0; i < sym.size(); ++i) {
-        for (size_t j = 0; j < sym.size(); ++j) {
-            if (i == 0 && j == 0) continue;
-            s = sym[i];
-            s += sym[j];
-            export_elem(s, os, true);
-        }
-    }
-}
-
-void MRFExporter::export_edge_weight(const MatrixXf& w, ostream& os) {
-    export_elem(w(0, 0), os);
-    for (int i = 0; i < w.rows(); ++i) {
-        for (int j = 0; j < w.cols(); ++j) {
-            if (i == 0 && j == 0) continue;
-            export_elem(w(i, j), os, true);
-        }
-    }
-}
-
-void MRFExporter::export_elem(const double& x, ostream& os, bool pre_sep) {
-    if (pre_sep) os << "\t";
-    if (x == 0) os << "*";
-    else os << setprecision(4) << x;
-}
-
-void MRFExporter::export_elem(const char& x, ostream& os, bool pre_sep) {
-    if (pre_sep) os << "\t";
-    os << x;
-}
-
-void MRFExporter::export_elem(const string& x, ostream& os, bool pre_sep) {
-    if (pre_sep) os << "\t";
-    os << x;
+    // Writing sequence profile
+    os.write((char*)model.get_psfm().data(), n * k * sizeof(float));
+    // Writing node weight
+    for (size_t i = 0; i < n; ++i)
+        os.write((char*)model.get_node(i).get_weight().data(), k * sizeof(float));
+    // Writing edge weight
+    for (EdgeIndexVector::const_iterator pos = edge_idxs.begin(); pos != edge_idxs.end(); ++pos)
+        os.write((char*)model.get_edge(pos->idx1, pos->idx2).get_weight().data(), k * k * sizeof(float));
 }
 
 MRF MRFImporter::import_model(istream& is, const Alphabet& abc) {
-    MRF model = import_header(is, abc);
-    is.clear();
-    is.seekg(0, is.beg);
-    import_body(is, model);
-    return model;
-}
-
-MRF MRFImporter::import_header(istream& is, const Alphabet& abc) {
-    string dummy, s;
-    size_t length;
+    size_t n, n2;
     string seq;
-    EdgeIndexVector eidxs;
-    while (is) {
-        is >> s;
-        if (s == "#") {
-            is >> s;
-            if (s == "EDGE") {
-                getline(is, dummy);
-                getline(is, dummy);
-                import_eidxs(is, eidxs);
-            }
-        } else if (s == "VER") {
-            getline(is, dummy);
-        } else if (s == "LENG") {
-            is >> length;
-            getline(is, dummy);
-        } else if (s == "SEQ") {
-            getline(is, dummy);
-            seq = import_seq(is, length);
-        }
+    is.read((char*)&n, sizeof(size_t));
+    std::getline(is, seq, '\0');
+    // Reading MRF architecture
+    is.read((char*)&n2, sizeof(size_t));
+    EdgeIndexVector edge_idxs;
+    size_t idx1, idx2;
+    for (size_t i = 0; i < n2; ++i) {
+        is.read((char*)&idx1, sizeof(size_t));
+        is.read((char*)&idx2, sizeof(size_t));
+        edge_idxs.push_back(EdgeIndex(idx1, idx2));
     }
-    MRF model(seq, abc, &eidxs);
+    MRF model(seq, abc, &edge_idxs);
+    size_t k = model.get_num_var();
+    // Reading profile
+    MatrixXf m(n, k);
+    is.read((char*)m.data(), n * k * sizeof(float));
+    model.set_psfm(m);
+    // Reading node weight
+    VectorXf v(k);
+    for (size_t i = 0; i < n; ++i) {
+        is.read((char*)v.data(), k * sizeof(float));
+        model.get_node(i).set_weight(v);
+    }
+    // Reading edge weight
+    m.resize(k, k);
+    for (EdgeIndexVector::const_iterator pos = edge_idxs.begin(); pos != edge_idxs.end(); ++pos) {
+        is.read((char*)m.data(), k * k * sizeof(float));
+        model.get_edge(pos->idx1, pos->idx2).set_weight(m);
+    }
     return model;
-}
-
-string MRFImporter::import_seq(istream& is, const size_t& length) {
-    string seq, s;
-    while (is) {
-        getline(is, s);
-        seq += s;
-        if (seq.size() >= length) break;
-    }
-    return seq;
-}
-
-void MRFImporter::import_eidxs(istream& is, EdgeIndexVector& eidxs) {
-    string s;
-    size_t idx1, idx2;
-    while (is) {
-        is >> s;
-        if (s == "//") break;
-        is >> idx1;
-        is >> s;
-        is >> idx2;
-        eidxs.push_back(EdgeIndex(idx1 - 1, idx2 - 1));
-        getline(is, s);
-    }
-}
-
-void MRFImporter::import_body(istream& is, MRF& model) {
-    string dummy, s;
-    size_t idx1, idx2;
-    size_t num_var = model.get_num_var();
-    while (is) {
-        is >> s;
-        if (s == "#") {
-            is >> s;
-            if (s == "NODE") {
-                getline(is, dummy);
-                getline(is, dummy);
-                size_t n = model.get_length();
-                for (size_t i = 0; i < n; ++i) {
-                    is >> dummy;
-                    is >> dummy;
-                    model.get_node(i).set_weight(import_node_weight(is, num_var));
-                }
-            } else if (s == "EDGE") {
-                getline(is, dummy);
-                getline(is, dummy);
-                size_t n = model.get_num_edge();
-                for (size_t i = 0; i < n; ++i) {
-                    is >> dummy;
-                    is >> idx1;
-                    is >> dummy;
-                    is >> idx2;
-                    model.get_edge(idx1 - 1, idx2 - 1).set_weight(import_edge_weight(is, num_var));
-                }
-            } else if (s == "PROFILE") {
-                getline(is, dummy);
-                getline(is, dummy);
-                size_t n = model.get_length();
-                MatrixXf psfm = MatrixXf::Zero(n, num_var);
-                for (size_t i = 0; i < n; ++i) {
-                    is >> dummy;
-                    is >> dummy;
-                    psfm.row(i) = import_psfm(is, num_var);
-                }
-                model.set_psfm(psfm);
-            }
-        }
-    }
-}
-
-VectorXf MRFImporter::import_node_weight(istream& is, const size_t& num_var) {
-    VectorXf w(num_var);
-    for (size_t i = 0; i < num_var; ++i) w(i) = import_elem(is);
-    return w;
-}
-
-MatrixXf MRFImporter::import_edge_weight(istream& is, const size_t& num_var) {
-    MatrixXf w(num_var, num_var);
-    for (size_t i = 0; i < num_var; ++i)
-        for (size_t j = 0; j < num_var; ++j)
-            w((int)i, (int)j) = import_elem(is);
-    return w;
-}
-
-VectorXf MRFImporter::import_psfm(istream& is, const size_t& num_var) {
-    VectorXf w(num_var);
-    for (size_t i = 0; i < num_var; ++i) w(i) = import_elem(is);
-    return w;
-}
-
-double MRFImporter::import_elem(istream& is) {
-    string s;
-    is >> s;
-    double d;
-    if (s == "*") d = 0;
-    else std::istringstream(s) >> d;
-    return d;
 }
 
 string format_seq(const string& seq, const size_t& width) {
--- src/mrfio.h
+++ src/mrfio.h
@@ -17,50 +17,11 @@ class EdgeIndexImporter {
 class MRFExporter {
   public:
     void export_model(const MRF& model, ostream& os);
-
-  protected:
-    void export_header(const MRF& model, ostream& os);
-    void export_body(const MRF& model, ostream& os);
-
-    void export_seq(const string& seq, const size_t& width, ostream& os);
-    void export_psfm(const VectorXf& w, ostream& os);
-    void export_node_symbol(const string& sym, ostream& os);
-    void export_node_weight(const VectorXf& w, ostream& os);
-    void export_edge_symbol(const string& sym, ostream& os);
-    void export_edge_weight(const MatrixXf& w, ostream& os);
-
-    FRIEND_TEST(MRFExporter_Test, test_export_seq);
-    FRIEND_TEST(MRFExporter_Test, test_export_node_symbol);
-    FRIEND_TEST(MRFExporter_Test, test_export_node_weight);
-    FRIEND_TEST(MRFExporter_Test, test_export_edge_symbol);
-    FRIEND_TEST(MRFExporter_Test, test_export_edge_weight);
-
-  private:
-    void export_elem(const double& x, ostream& os, bool pre_sep=false);
-    void export_elem(const char& x, ostream& os, bool pre_sep=false);
-    void export_elem(const string& x, ostream& os, bool pre_sep=false);
 };
 
 class MRFImporter {
   public:
     MRF import_model(istream& is, const Alphabet& abc);
-
-  protected:
-    MRF import_header(istream& is, const Alphabet& abc);
-    string import_seq(istream& is, const size_t& length);
-    void import_eidxs(istream& is, EdgeIndexVector& eidxs);
-    void import_body(istream& is, MRF& model);
-    VectorXf import_node_weight(istream& is, const size_t& num_var);
-    MatrixXf import_edge_weight(istream& is, const size_t& num_var);
-    VectorXf import_psfm(istream& is, const size_t& num_var);
-
-    FRIEND_TEST(MRFImporter_Test, test_import_seq);
-    FRIEND_TEST(MRFImporter_Test, test_import_node_weight);
-    FRIEND_TEST(MRFImporter_Test, test_import_edge_weight);
-    FRIEND_TEST(MRFImporter_Test, test_import_psfm);
-
-  private:
-    double import_elem(istream& is);
 };
 
 ostream& operator<<(ostream& os, const MRF& model);
