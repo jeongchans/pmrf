ebf585f96325df37b9264bf640c053ec6034fd23
Chan-Seok Jeong <csjeong@kaist.ac.kr>
2016-04-27 23:50:21 +0900

    chore: show optimality term for debugging

--- src/lbfgs.cpp
+++ src/lbfgs.cpp
@@ -6,11 +6,15 @@
 
 int LBFGS::ObjectiveFunction::progress(const lbfgsfloatval_t *x, const lbfgsfloatval_t *g, const lbfgsfloatval_t fx, const lbfgsfloatval_t xnorm, const lbfgsfloatval_t gnorm, const lbfgsfloatval_t step, int n, int k, int ls) {
 #ifdef _DEBUG_
+    lbfgsfloatval_t opt_cond;
+    if (xnorm < 1.) opt_cond = gnorm;
+    else opt_cond = gnorm / xnorm;
     std::clog << "[L-BFGS progress]"
               << "  iteration = " << k
               << ", obj_val = " << fx
               << ", xnorm = " << xnorm
               << ", gnorm = " << gnorm
+              << ", opt_cond = " << opt_cond
               << ", step = " << step
               << ", ls = " << ls 
               << std::endl;
